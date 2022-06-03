7c13ac9130eb70821f97fa2c03d6fff430e14395
Praful <prafulrakhade02@gmail.com>
2022-06-03 15:25:02 +0530

    [praful] Added UC_15 Program file

---
+++ AddressBook.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace AddressBookProblemWithJSON
+{
+    /// <summary>
+    /// Person Class for List element
+    /// </summary>
+    public class Person
+    {
+        public string FName;
+        public string LName;
+        public string Address;
+        public long PhoneNumber;
+
+        public string Result()
+        {
+            return "Name is: " + FName + " " + LName + "\n Address: " + Address + "\n Phone: " + PhoneNumber;
+        }
+    }
+
+    /// <summary>
+    /// Address Book Class
+    /// </summary>
+    public class AddressBook
+    {
+        //Creating List 
+        public List<Person> person = new List<Person>();
+
+
+        //Address Book method
+        public AddressBook()
+        {
+            string json = File.ReadAllText(@"C:\Users\HP\Bridgelabz-145\Assignments_\Day 27\AddressBookProblemWithJSON\AddressBookProblemWithJSON\Records.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            else
+                person = new List<Person>();
+        }
+
+        //Display Method
+        public void Display()
+        {
+            if (person.Count == 0)
+            {
+                Console.WriteLine("Please add Some Contact list First");
+            }
+            Console.WriteLine("Welcome to Program");
+            foreach (Person per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        //Adding Contact list method
+        public void addPerson(Person p)
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"C:\Users\HP\Bridgelabz-145\Assignments_\Day 27\AddressBookProblemWithJSON\AddressBookProblemWithJSON\Records.json", jsonData);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace AddressBookProblemWithJSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Problem with JSON \n\n");
+            AddressBook manager = new AddressBook();
+
+            //Infinite Loop
+            while (true)
+            {
+                Console.WriteLine("\nPlease Select any one Operation: ");
+                Console.WriteLine("1: To add Details to Address Book");
+                Console.WriteLine("2: To Display All Contacts of Address Book");
+                Console.WriteLine("3: To Exit");
+                int read = int.Parse(Console.ReadLine());
+                switch (read)
+                {
+                    //For Adding Details to Address Book
+                    case 1:
+                        {
+                            Person person = new Person();
+                            Console.WriteLine("Enter First Name: ");
+                            string fname = Console.ReadLine();
+                            person.FName = fname;
+                            Console.WriteLine("Enter Last Name: ");
+                            string lname = Console.ReadLine();
+                            person.LName = lname;
+                            Console.WriteLine("Enter Address: ");
+                            string address = Console.ReadLine();
+                            person.Address = address;
+                            Console.WriteLine("Enter Phone Number: ");
+                            long number = long.Parse(Console.ReadLine());
+                            person.PhoneNumber = number;
+                            manager.addPerson(person);
+                        }
+                        break;
+                    //Display contact list of addressbook
+                    case 2:
+                        {
+                            manager.Display();
+                        }
+                        break;
+                        //for exit from infinite loop
+                    case 3:
+                        {
+                            Environment.Exit(0);
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Please Select any one operation ");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
